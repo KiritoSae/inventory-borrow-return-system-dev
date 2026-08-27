cac102d0cb255ce160382d8508e4ec7d5b5c9fc5
KiritoSae <perezerdyn@gmail.com>
2026-08-27 13:26:11 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,42 @@
+<?php
 
+session_start();
+
+/*
+ * Remove all session variables.
+ */
+$_SESSION = [];
+
+
+/*
+ * Delete the session cookie.
+ */
+if (ini_get("session.use_cookies")) {
+
+    $params = session_get_cookie_params();
+
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+
+/*
+ * Destroy the session.
+ */
+session_destroy();
+
+
+/*
+ * Return to login.
+ */
+header("Location: login.php");
+exit;
+
+?>
